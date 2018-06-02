576ee523a5556c6a2ecf62d437b3db207d34bd8c
ChinaskiJr <robin.colombier@gmail.com>
2018-06-02 18:22:42 +0200

    add an autoloader for classes

--- controller/controller.php
+++ controller/controller.php
@@ -1,6 +1,27 @@
 <?php 
-require_once('model/CharacterManager.php');
-require_once('classes/Character.php');
+/**
+ * Auto-loader for classes
+ * 
+ * @param  string $class The name of the class 
+ * 
+ * @return void
+ */
+function autoload($class) {
+	$class = explode('\\', $class);
+	$class = end($class);
+	if (file_exists($class . '.php')) {
+		require_once $class . '.php';
+	} else
+		if (file_exists('model/' . $class . '.php')) {
+			require_once('model/' . $class . '.php');
+		} elseif (file_exists('classes/' . $class . '.php')) {
+			require_once('classes/' . $class . '.php');
+		} else  {
+			trigger_error('Unable to find the class', E_USER_WARNING);
+		}
+}
+spl_autoload_register('autoload');
+
 /**
  * Send an array that contains a character's attributes
  * 
@@ -13,7 +34,8 @@ require_once('classes/Character.php');
  */
 function sendCharacterAttributesToTheView($characterId) {
 	$characterManager = new \Openclassrooms\Mini_fight_game\Model\CharacterManager();
-	$characterAttributes = $characterManager->getCharacterAttributesFromDb($characterId);
+	$characterAttributes = $characterManager->getCharacterAttributes($characterId);
 	$character = new \Openclassrooms\Mini_fight_game\Classes\Character($characterAttributes);
 	require_once('view/frontend/displayCharacterAttribute.php');
 }
+
