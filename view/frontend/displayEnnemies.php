dd6368d5488a853bb51ac33e4ad05aa2b19969b3
ChinaskiJr <robin.colombier@gmail.com>
2018-06-05 12:37:43 +0200

    create the link for fighting

--- view/frontend/displayEnnemies.php
+++ view/frontend/displayEnnemies.php
@@ -4,10 +4,9 @@ ob_start();
 echo '<div>You are facing :';
 
 foreach ($charactersAttributes as $key => $value) {
-	echo '<p>' . htmlspecialchars($value->name()) . "</p>";
-
+	echo '<p><a href="?hit='. $value->id() . '">' . htmlspecialchars($value->name()) . '</a> (damages : ' . $value->damages() . ')</p>';
 }
 echo '</div>';
 
 $content = ob_get_clean();
-require('template.php'); 
+require('template.php');
