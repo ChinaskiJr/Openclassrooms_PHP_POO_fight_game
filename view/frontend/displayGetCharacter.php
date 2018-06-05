8dfdab5fc375b03b5a432b2b01f804dd2714a5d2
ChinaskiJr <robin.colombier@gmail.com>
2018-06-05 15:07:47 +0200

    frontend improvements

--- view/frontend/displayGetCharacter.php
+++ view/frontend/displayGetCharacter.php
@@ -1,9 +1,9 @@
 <?php
 ob_start(); 
 
-echo 'Now playing with ' . htmlspecialchars($charac->name()) . '. Have a nice fight !' . "\r";
+echo 'Playing with ' . htmlspecialchars($charac->name()) . ' - <a href="?disconnect=1">Disconnect</a><hr />';
 echo '<h3>Character\'s attributes</h3>';
-echo (htmlspecialchars($charac->name()) . ' has ' . $charac->strenght() . ' of strenght, took ' . $charac->damages() . ' damages, is level ' . $charac->level() . ' and has ' . $charac->experience() . ' points of experience.<br />');
+echo (htmlspecialchars($charac->name()) . ' has ' . $charac->strenght() . ' of strenght, took ' . $charac->damages() . ' damages, is level ' . $charac->level() . ' and has ' . $charac->experience() . ' points of experience.<br /><hr />');
 
 $content = ob_get_clean();
 require('template.php'); 
--- view/frontend/displayNewCharacter.php
+++ view/frontend/displayNewCharacter.php
@@ -1,7 +1,8 @@
 <?php
 ob_start(); 
 
-echo htmlspecialchars($charac->name()) . ' has been created ! May you live long !';
+echo htmlspecialchars($charac->name()) . ' has been created ! May you live long !<br />';
+echo 'Now, go to <a href="">fight</a> !';
 
 $content = ob_get_clean();
 require('template.php'); 
